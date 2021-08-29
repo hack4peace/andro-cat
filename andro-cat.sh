fd21c45d694763f546f4bc7982640117631f1d15
Hack4peace <85699568+hack4peace@users.noreply.github.com>
2021-08-29 15:42:48 +0530

    Create andro-cat.sh

---
+++ andro-cat.sh
@@ -0,0 +1,20 @@
+echo -e ",_     _"
+echo -e " |\\_,-~/"
+echo -e " / _  _ |    ,--."
+echo -e "(  @  @ )   / ,-'"
+echo -e " \  _T_/-._( ("
+echo -e " /         `. \"
+echo -e "|         _  \ |"
+echo -e " \ \ ,  /      |"
+echo -e "  || |-_\__   /"
+echo -e " ((_/`(____,-'"
+sleep 5
+adb pull /sdcard/Download
+adb pull /sdcard/DCMI
+adb pull /sdcard/Android
+adb pull /sdcard/Movies
+adb pull /sdcard/Music
+adb pull /sdcard/Notifications
+adb pull /sdcard/Pictures
+adb pull /sdcard/Podcasts
+adb pull /sdcard/Ringtones
